8522e061b500bd00c0fafb2b7d3ca9a9cc157510
mariahsiao <thatsjustmeiwant@gmail.com>
2016-07-30 03:12:50 +0800

    if you are not the group owner then won't show edit button

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,7 +4,7 @@ class Group < ActiveRecord::Base
 
   has_many :posts
 
-  belons_to :owner, class_name: "User", foreign_key: :user_id
+  belongs_to :owner, class_name: "User", foreign_key: :user_id
 
   def editable_by?(user)
     user && user == owner
