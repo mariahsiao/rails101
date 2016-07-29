23a717930e98fb060115ae3e829f3c8634d48e26
mariahsiao <thatsjustmeiwant@gmail.com>
2016-07-30 03:38:31 +0800

    create group_user and set relationship with group.rb & user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :groups
   has_many :posts
+
+  has_many :group_users
+  has_many :participated_groups, through: :group_users, source: :group
 end
