35069e8bdd629560d19aae86770f8951c2e65f1b
mariahsiao <thatsjustmeiwant@gmail.com>
2016-07-30 15:28:49 +0800

    post_count in my group bug fix

--- app/controllers/account/groups_controller.rb
+++ app/controllers/account/groups_controller.rb
@@ -2,6 +2,6 @@ class Account::GroupsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @groups = current_user.participated_groups.order("post_count DESC")
+    @groups = current_user.participated_groups.order("posts_count DESC")
   end
 end
